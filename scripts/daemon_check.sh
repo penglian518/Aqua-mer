2067b054e1c472d17d94b5cf70c39560180c0e33
Peng Lian <penglian518@foxmail.com>
2019-06-12 11:21:49 -0400

    Check PBS daemons periodically.

--- scripts/daemon_check.sh
+++ scripts/daemon_check.sh
@@ -14,3 +14,21 @@ else
     su p6n -c 'cd /home/p6n/workplace/website/cyshg; /home/p6n/anaconda2/bin/python ./daemon.py > log 2>&1 &'
 fi
 
+# Check pbs daemons
+Check_pbs(){
+    Num_proc=`ps -f -u root | grep $1 | grep -v grep | wc -l`
+
+    if [ $Num_proc -eq 1 ]; then
+        #echo `date`: "$1 is running!" >> $checking_log
+        :
+    else
+        echo `date`: "$1 is dead! Restart it now." >> $checking_log
+        /usr/sbin/$1 >> $checking_log 2>&1
+    fi
+}
+
+# check the daemons pbs_server pbs_sched pbs_mom
+for i in pbs_server pbs_sched pbs_mom
+do
+    Check_pbs $i;
+done
