bfbdace3900630286840cb3aed7cbf7d9ac40262
Peng Lian <penglian518@foxmail.com>
2019-11-08 10:10:25 -0500

    Updated the backup schedule

--- scripts/daemon_check.sh
+++ scripts/daemon_check.sh
@@ -9,6 +9,10 @@ checking_log="/home/p6n/workplace/website/cyshg/daemon_checking.log"
 if [ $Num_proc -eq 1 ]; then
     #echo `date`: 'daemon.py is running!' >> $checking_log
     :
+elif [ $Num_proc -gt 1 ]; then
+    for i in `ps -f -u p6n | grep daemon.py | grep -v grep | awk '{print $2}'`; do kill $i; done
+    echo `date`: 'More than one daemon.py is running! Clean all and restart now.' >> $checking_log
+    su p6n -c 'cd /home/p6n/workplace/website/cyshg; /home/p6n/anaconda2/bin/python ./daemon.py > log 2>&1 &'
 else
     echo `date`: 'daemon.py is dead! Restart it now.' >> $checking_log
     su p6n -c 'cd /home/p6n/workplace/website/cyshg; /home/p6n/anaconda2/bin/python ./daemon.py > log 2>&1 &'
